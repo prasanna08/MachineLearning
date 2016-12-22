e40a189faaa4048a3ddc08ebaccfe9a67db41e3b
prasanna08 <prasannapatil08@gmail.com>
2016-12-22 15:01:46 +0530

    Changes to preprocess function.

--- preprocess.py
+++ preprocess.py
@@ -2,5 +2,5 @@ import numpy as np
 
 """This file contains some functions related to preprocessing."""
 
-def get_labels(output_labels):
-	return (np.unique(output_labels) == output_labels[:, None]).astype(np.float32)
+def onehot(output_labels):
+	return np.squeeze((np.unique(output_labels) == output_labels[:, None]).astype(np.float32))
