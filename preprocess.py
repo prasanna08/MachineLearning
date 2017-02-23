d6ff366c4ec531d4ffdc6ccea12d4ea53373b9de
prasanna08 <prasannapatil08@gmail.com>
2017-02-23 23:14:58 +0530

    Implement xavier initialization functions.

--- preprocess.py
+++ preprocess.py
@@ -4,3 +4,11 @@ import numpy as np
 
 def onehot(output_labels):
 	return np.squeeze((np.unique(output_labels) == output_labels[:, None]).astype(np.float32))
+
+def xavier_init(shape, fan_in, fan_out):
+	dev = np.sqrt(6.0 / (fan_in + fan_out))
+	return np.random.uniform(-dev, dev, shape)
+
+def xavier_init_normal(shape, fan_in, fan_out):
+	dev = np.sqrt(3.0 / (fan_in + fan_out))
+	return np.random.normal(size=shape, scale=dev)
