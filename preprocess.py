747943d896cd73732d6585f6508cf26185872fc7
prasanna08 <prasannapatil08@gmail.com>
2017-03-22 13:51:48 +0530

    Calculate fan_in and fan_out from shape itself.

--- preprocess.py
+++ preprocess.py
@@ -5,11 +5,16 @@ import numpy as np
 def onehot(output_labels):
 	return np.squeeze((np.unique(output_labels) == output_labels[:, None]).astype(np.float32))
 
-def xavier_init(shape, fan_in, fan_out):
+def get_fans(shape):
+	return np.prod(shape[:-1]), shape[-1]
+
+def xavier_init(shape):
+	fan_in, fan_out = get_fans(shape)
 	dev = np.sqrt(6.0 / (fan_in + fan_out))
 	return np.random.uniform(-dev, dev, shape)
 
-def xavier_init_normal(shape, fan_in, fan_out):
+def xavier_init_normal(shape):
+	fan_in, fan_out = get_fans(shape)
 	dev = np.sqrt(3.0 / (fan_in + fan_out))
 	return np.random.normal(size=shape, scale=dev)
 
